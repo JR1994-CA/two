5479a4ec65c597869667e0c1a8b06b0bc1434b28
Duality <jramirezarroyo21@gmail.com>
2021-06-29 20:52:22 -0700

    Chip Schema unfinished

--- models/Chips.js
+++ models/Chips.js
@@ -2,14 +2,30 @@ const { Schema, model, Types } = require('mongoose');
 
 const Chip = new Schema(
     {
-        count: {
-            type: Integer,
-            default: 0
-        },
+        count: { type: Integer, default: 0 },
         cost: {
-            board: 0,
-            comp: 1000,
-            chip: 0
-        }
+            board:{ type: Integer, default: 0 },
+            comp: { type: Integer, default: 0 },
+            chip: { type: Integer, default: 0 },
+            cpu: { type: Integer, default: 0 }
+        },
+        mod: {
+            click: { type: Integer, default: 0 },
+            per: { type: Integer, default: 0 }
+        },
+        curUpgrade:{ type: Integer, default: 0 },
+        isHide: { type: Boolean, default: true }
+        stats: [StatsSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-)
+);
+
+Chip.virtual('updateStat').get(function() {
+
+});
