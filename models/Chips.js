9ede5c2b9b4d9537d2c2daaafda668d3cbbb534b
Duality <jramirezarroyo21@gmail.com>
2021-06-29 21:28:32 -0700

    Chip Model missing some virtuals

--- models/Chips.js
+++ models/Chips.js
@@ -1,15 +1,39 @@
 const { Schema, model, Types } = require('mongoose');
+const stats = require('./Stats');
 
 const Chip = new Schema(
     {
-        count: {
-            type: Integer,
-            default: 0
-        },
+        count: { type: Integer, default: 0 },
         cost: {
-            board: 0,
-            comp: 1000,
-            chip: 0
+            board:{ type: Integer, default: 0 },
+            comp: { type: Integer, default: 0 },
+            chip: { type: Integer, default: 0 },
+            cpu: { type: Integer, default: 0 }
+        },
+        mod: {
+            click: { type: Integer, default: 0 },
+            per: { type: Integer, default: 0 }
+        },
+        curUpgrade:{ type: Integer, default: 0 },
+        isHide: { type: Boolean, default: true },
+        currStats: {
+            type: [stats],
+            get: currStats => stats[currStats]
         }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-)
+);
+
+Chip.virtual('getStats').get(function() {
+    return this.currStats;
+})
+
+const Chip = model('Chip', Chip);
+
+module.exports = Chip;
