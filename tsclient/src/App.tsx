b44dab50b57624a0d27e3b3358f391fe4586c538
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-07-01 03:27:21 -0700

    added a drawer to hold login/signup

--- tsclient/src/App.tsx
+++ tsclient/src/App.tsx
@@ -4,34 +4,36 @@ import {Grid} from "@material-ui/core";
 import NavBar from "./components/Navbar";
 import pie from "./OtherStuff/Holding"
 import Footer from "./components/Footer";
-
+import Drawer from "./components/Layout";
 
 function App() {
 
   return (
       <>
+
         <NavBar/>
         <Divider/>
-        <Grid container>
-          <Grid item>
-            {/*Dynamically input blocks?*/}
-            {/*<Block curr_block={"Transistor"} costFun={(upgrade) => (upgrade + 20) * 2} curr_count={objodata}/>*/}
-            {/*<Block curr_block={"Boards"} costFun={(upgrade) => (upgrade + 20) * 2} curr_count={objodata}/>*/}
-            <Block data={pie.trans} pie={pie}/>
-            <Block data={pie.chips} pie={pie}/>
-            <Block data={pie.boards} pie={pie}/>
-            <Block data={pie.cpus} pie={pie}/>
+          <Grid container>
+              <Grid item>
+                  {/*Dynamically input blocks?*/}
+                  {/*<Block curr_block={"Transistor"} costFun={(upgrade) => (upgrade + 20) * 2} curr_count={objodata}/>*/}
+                  {/*<Block curr_block={"Boards"} costFun={(upgrade) => (upgrade + 20) * 2} curr_count={objodata}/>*/}
+                  <Block data={pie.trans} pie={pie}/>
+                  <Block data={pie.chips} pie={pie}/>
+                  <Block data={pie.boards} pie={pie}/>
+                  <Block data={pie.cpus} pie={pie}/>
+              </Grid>
+              <Grid item>
+                  <Block data={pie.robot} pie={pie}/>
+                  <Block data={pie.fortyTwo} pie={pie}/>
+                  <Block data={pie.rowboat} pie={pie}/>
+                  <Block data={pie.phonebox} pie={pie}/>
+              </Grid>
 
           </Grid>
-                <Grid item>
-                    <Block data={pie.robot} pie={pie}/>
-                    <Block data={pie.fortyTwo} pie={pie}/>
-                    <Block data={pie.rowboat} pie={pie}/>
-                    <Block data={pie.phonebox} pie={pie}/>
-                </Grid>
+<Drawer/>
+        {/*<Footer/>*/}
 
-        </Grid>
-        <Footer/>
       </>
   );
 }
--- tsclient/src/components/Block/index.tsx
+++ tsclient/src/components/Block/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Divider, Grid, Tooltip} from "@material-ui/core";
 import {Button} from "@material-ui/core";
 import {useState} from "react";
-import {button} from "../../OtherStuff"
+// import {button} from "../../OtherStuff"
 
 function Block(props:any)
 {
@@ -18,37 +18,35 @@ function Block(props:any)
     return (
 
         <Grid container className="">
-    {/*Row 1*/}
-    <Grid container
-    direction="row"
-    justify="space-evenly"
-    alignItems="center">
-        <h3>{data.name}<span className=""> {data.qty}</span></h3>
-</Grid>
-<Divider/>
+            {/*Row 1*/}
+            <Grid container
+                  direction="row"
+                  justify="space-evenly"
+                  alignItems="center">
+                <h3>{data.name}<span className=""> {data.qty}</span></h3>
+            </Grid>
+            <Divider/>
 {/*Row 2*/}
-<Grid container
-    direction="row"
-    justify="space-evenly"
-    alignItems="center">
-        <Grid item direction="column">
-<div className="">click: {data.perClick}</div>
-<div className="">
-<Button color="secondary" variant="outlined" aria-label="outlined secondary" onClick={() =>
-    {
-        data.changeQty(1);
-        setFoo(++foo);//forcing update
-        console.log(data.qty)
-    }}>Do Click</Button>
-</div>
-<div className="">
-<Tooltip title={disp}>
+        <Grid container
+            direction="row"
+            justify="space-evenly"
+            alignItems="center">
+
+            <Grid item direction="column">
+                <div className="">click: {data.perClick}</div>
+                <div className="">
+                <Button color="secondary" variant="outlined" aria-label="outlined secondary" onClick={() => {
+                    data.changeQty(1);
+                    setFoo(++foo);//forcing update
+                    console.log(data.qty)
+                }}>Do Click</Button>
+                </div>
+                <div className="">
+                    <Tooltip title={disp}>
 <span>
     <Button disabled={data.qty <= data.costs.chips()} color="secondary" variant="outlined"
-    aria-label="outlined secondary" onClick={() =>
-    {
-        if (data.qty > data.costs.chips())
-        {
+            aria-label="outlined secondary" onClick={() => {
+        if (data.qty > data.costs.chips()) {
             data.addLvl();
             data.changeQty(-data.costs.chips());
             setFoo(0);
@@ -56,7 +54,7 @@ function Block(props:any)
         }
     }}>{data.qty <= data.costs.chips() ? "Need more Chips" : "Upgrade"}</Button>
 </span>
-</Tooltip>
+                    </Tooltip>
 </div>
 </Grid>
 </Grid>
