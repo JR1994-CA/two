c7727a7e4228fd6fddcc032c48d4f0569e79190e
Duality <jramirezarroyo21@gmail.com>
2021-07-01 20:03:32 -0700

    User Schema revworked with Bcrypt

--- models/User.js
+++ models/User.js
@@ -1,28 +1,32 @@
-const { User, model } = require('mongoose');
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
-const clickable = require('./Clickables');
-const transistor = require(./)
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+})
 
-const User = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            maxlength: 50,
-            trim: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true,
-            maxlength: 50,
-            trim: true,
-            unique: true
-        },
+UserSchema.pre("save", function (next) {
+    const user = this
 
+    if (this.isModified("password") || this.isNew) {
+        bcrypt.genSalt(10, function (saltError, salt) {
+            if (saltError) {
+                return next(saltError)
+            } else {
+                bcrypt.hash(user.password, salt, function(hashError, hash) {
+                    if (hashError) {
+                        return next(hashError)
+                    }
 
-
+                    user.password = hash
+                    next()
+                })
+            }
+        })
+    } else {
+        return next()
     }
+})
 
-
-);
+module.exports = mongoose.model("User", UserSchema)
