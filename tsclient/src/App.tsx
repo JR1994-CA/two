9c826299f3305e6387dabe548172dfa87a7e4f2a
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-07-03 20:13:55 -0700

    clean up app.js

--- tsclient/src/App.tsx
+++ tsclient/src/App.tsx
@@ -2,21 +2,16 @@ import {Container, Divider} from "@material-ui/core";
 import NavBar from "./components/Navbar";
 import Drawer from "./components/Layout/index";
 import Main from './components/Draw/main';
-import Login from "./components/LoginSignup/Login";
-import Signup from "./components/LoginSignup/Signup";
+import ContactComponent from './components/LoginSignup/ContactModal';
 import SignInOutContainer from "./components/LoginSignup/Container";
 import {Switch,Route,Link} from 'react-router-dom';
 function App() {
     return (
         <Drawer>
             <Switch>
-                {/*<NavBar/>*/}
-                {/*<Divider/>*/}
-                {/*<Login/>*/}
-                {/*<Signup/>*/}
                 <Route path='/SigninOut' component={SignInOutContainer}/>
+                <Route path='/Contact' component={ContactComponent}/>
                 <Route path='/' component={Main}/>
-                {/*<Drawer/>*/}
             </Switch>
         </Drawer>
     );
