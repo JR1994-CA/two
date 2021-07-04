5b4e920a1c4843f0f0f7369449276544e4ff0583
kingly77 <shelbygj97@gmail.com>
2021-07-03 23:50:32 -0700

    changed draw to be usefullish

--- tsclient/src/components/Unlocks/uDrawer.tsx
+++ tsclient/src/components/Unlocks/uDrawer.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+
+import AttachMoney from '@material-ui/icons/AttachMoney';
 
 const drawerWidth = 240;
 
@@ -56,9 +51,10 @@ export default function UnlocksDrawer() {
             >
                 <div className={classes.toolbar} />
                 <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    {['Chips', 'Boards', 'Cpus', '42','RowBoat','PhoneBooth'].map((text, index) => (
+
+                        <ListItem button={true} onClick={()=>console.log("test")} key={text} >
+                            <ListItemIcon><AttachMoney/></ListItemIcon>
                             <ListItemText primary={text} />
                         </ListItem>
                     ))}
