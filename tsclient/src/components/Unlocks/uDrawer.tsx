c10d773f7d184a795e2253f7c8d844d80a3f3a94
kingly77 <shelbygj97@gmail.com>
2021-07-04 00:40:09 -0700

    changed drawer to be usefullish

--- tsclient/src/components/Unlocks/uDrawer.tsx
+++ tsclient/src/components/Unlocks/uDrawer.tsx
@@ -5,9 +5,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-
 import AttachMoney from '@material-ui/icons/AttachMoney';
-
 
 const drawerWidth = 190;
 
