104bbf89323189913dcf342d92fca41e4236cdb4
kingly77 <shelbygj97@gmail.com>
2021-07-05 15:24:27 -0700

    ql Start

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+const {graphqlHTTP} = require ("express-graphql");
 const expr = require('express')
-
-
 const def = require('./routes');
 const path = require('path');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4100;
 const app = expr();
-app.engine('handlebars', hdbar());
-app.set('view engine', 'handlebars');
+const ql = require("./routes/GraphQl/Ql")
+//app.engine('handlebars', hdbar());
+//app.set('view engine', 'handlebars');
 
 app.use(expr.urlencoded({ extended: true }))
 app.use(expr.json())
 
 app.use(expr.static(path.join(__dirname, 'public')));
 app.use(expr.static(path.join(__dirname, 'public/control')));
+
+app.use('/graphql', graphqlHTTP({
+    schema: ql.schema,
+    rootValue: ql.root,
+    graphiql: true,
+}));
 
 //Routing
 app.use('/', def);
