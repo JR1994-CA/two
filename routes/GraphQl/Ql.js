d56c0f1247a13509e0a858a116d886ffa8b586a6
kingly77 <shelbygj97@gmail.com>
2021-07-05 15:24:27 -0700

    ql Start

---
+++ routes/GraphQl/Ql.js
@@ -0,0 +1,22 @@
+const express = require('express');
+let {graphqlHTTP} = require('express-graphql');
+let {buildSchema} = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+  
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+    hello: () =>
+    {
+        return 'Hello world!';
+    }
+};
+
+module.exports = {schema, root}
