b6e014ddc9049eed59eb77f688af7918b6e179ee
Duality <jramirezarroyo21@gmail.com>
2021-07-05 15:14:47 -0700

    Back to the drawing board

---
+++ models/SaveTables/index.js
@@ -0,0 +1,6 @@
+const savingUpgrade = require('./saveUpgrade');
+const savingUnit = require('./saveUnit');
+const savePerSec = require ('./savePerSec');
+const savePerClick = require('./savePerClick');
+const isHidden = require('./isHid');
+module.exports = {savUp , savunit, savps , savpc , savhid}
