de3b7295d62b357c5d87e90137fcfbb06a24e125
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-07-07 02:21:00 -0700

    updated similar to 42 to rest of the classes in Upgrades.tsx

--- tsclient/src/OtherStuff/Data/Upgrades.tsx
+++ tsclient/src/OtherStuff/Data/Upgrades.tsx
@@ -3,28 +3,28 @@ import upgrade from "./GenericUpgrade";
 
 export class UfortyTwo extends upgrade {
 
-    defcost:number = 100 * this.qty+1;
+    defcostTrans:number = 100 * this.qty+1;
 
     uButton = () => {
         pie.trans.perSec = pie.fortyTwo.qty;
         this.addLvl();
     }
 
-    bButton=()=>
-    {
-        if(pie.trans.qty > this.defcost) {
-            console.log(this.defcost)
-            pie.trans.changeQty(-this.defcost);
-            this.qty++
-            this.defcost = 100 * this.qty+1
-            return{did:true,mess:"Hi"}
-        }
-        return{did:false,mess:"Bye"}
-    }
+    // bButton=()=>
+    // {
+    //     if(pie.trans.qty > this.defcost) {
+    //         console.log(this.defcost)
+    //         pie.trans.changeQty(-this.defcost);
+    //         this.qty++
+    //         this.defcost = 100 * this.qty+1
+    //         return{did:true,mess:"Hi"}
+    //     }
+    //     return{did:false,mess:"Bye"}
+    // }
 
     checkCCost= ()=>
     {
-        if(pie.trans.qty > this.defcost)
+        if(pie.trans.qty > this.defcostTrans)
             return{did:true,mess:"Buy"}
         return {did:false,mess:'Locked'}
     }
@@ -34,9 +34,13 @@ export class UfortyTwo extends upgrade {
 //Rowboat
 export class URow extends upgrade
 {
-
+    defcostTrans:number = 100 * this.qty+1;
+    defcostChips:number = 50 * this.qty+1;
+    defcostBoards:number =25 * this.qty+1;
     checkCost = ()=>
     {
+        if(pie.trans.qty > this.defcostTrans&&pie.chips.qty>this.defcostChips&&pie.boards.qty>this.defcostBoards)
+            return{did:true,mess:"Buy"}
         return{did:true ,mess:"Unlock"}
     }
 
@@ -50,24 +54,34 @@ export class URow extends upgrade
 
 export class UPhoneBox extends upgrade
 {
+    defcostTrans:number = 100 * this.qty+1;
+    defcostChips:number = 50 * this.qty+1;
     uButton=()=>{
         pie.chips.perSec = pie.phonebox.qty ;
         this.addLvl();
     }
     checkCost = ()=>
     {
+        if(pie.trans.qty > this.defcostTrans&&pie.chips.qty>this.defcostChips)
+            return{did:true,mess:"Buy"}
         return{did:true ,mess:"Unlock"}
     }
 }
 
 export class URobot extends upgrade
 {
+    defcostTrans:number = 100 * this.qty+1;
+    defcostChips:number = 50 * this.qty+1;
+    defcostBoards:number =25 * this.qty+1;
+    defcostCpus:number=10*this.qty+1;
     uButton=()=>{
         pie.cpus.perSec = pie.robot.qty;
         this.addLvl();
     }
     checkCost = ()=>
     {
+        if(pie.trans.qty > this.defcostTrans&&pie.chips.qty>this.defcostChips&&pie.boards.qty>this.defcostBoards&&pie.cpus.qty>this.defcostCpus)
+            return{did:true,mess:"Buy"}
         return{did:true ,mess:"Unlock"}
     }
 }
