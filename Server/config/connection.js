75700a6c9a3fa92efaccb27e456fb35aab45221f
Duality <jramirezarroyo21@gmail.com>
2021-07-08 16:51:10 -0700

    LETS GOOOO!!!!

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const password = require('../secrets')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Student2:' + password + '@cluster0.equy6.mongodb.net/FinalProject?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
