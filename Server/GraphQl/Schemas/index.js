72749951d1779d50c2573d39fb68d50ffde41e9d
Duality <jramirezarroyo21@gmail.com>
2021-07-07 20:41:31 -0700

    Connecting both front and backend

--- Server/GraphQl/Schemas/index.js
+++ Server/GraphQl/Schemas/index.js
@@ -2,10 +2,15 @@ const statRoot = require('./statRoot')
 const userRoot = require('./userRoot')
 const statTypeDef = require('./statTypeDefs')
 const userTypeDef = require('./userTypeDefs')
+const clickableTypeDef = require('./clickableTypeDef')
+const clickableRoots = require('./clickableRoots')
+
 
 module.exports = {
+    userTypeDef,
+    statTypeDef,
+    clickableTypeDef,
     statRoot,
     userRoot,
-    userTypeDef,
-    statTypeDef
+    clickableRoots,
 }
--- Server/index.js
+++ Server/index.js
@@ -1,50 +1,57 @@
-const {graphqlHTTP} = require ("express-graphql");
-const expr = require('express')
-const def = require('./routes');
-const path = require('path');
-const port = process.env.PORT || 4100;
-const app = expr();
-const ql = require("./GraphQl/Ql")
+const express = require('express')
 const mongoo = require('mongoose')
-const roots = require('./GraphQl/Schemas')
-
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+
+const { clickableRoots,
+    clickableTypeDef,
+    statRoot,
+    statTypeDef,
+    userRoot,
+    userTypeDef} = require('./GraphQl/Schemas');
+
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-const { statsschem, UserSchem } = require('./GraphQl/Schemas')
-const {graphql} = require('graphql')
+
+const PORT = process.env.PORT || 4100;
+const app = express();
+
+
+
+
+
+
 
 
 
 const server = new ApolloServer({
-    statschem,
-    UserSchem
+    userTypeDef,
+    clickableTypeDef,
+    statTypeDef,
+    statRoot,
+    clickableRoots,
+    userRoot,
+    context: authMiddleware
 });
 
 server.applyMiddleware({ app });
-//app.engine('handlebars', hdbar());
-//app.set('view engine', 'handlebars');
-mongoo.connect('mongodb://localhost/pie',{ useNewUrlParser: true, useUnifiedTopology: true })
 
-app.use(expr.urlencoded({ extended: true }))
-app.use(expr.json())
+//mongoo.connect('mongodb://localhost/pie',{ useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
 console.log(path.join(__dirname,"../tsclient/build/"));
-app.use(expr.static(path.join(__dirname, '../tsclient/build/')));
+app.use(express.static(path.join(__dirname, '../tsclient/build/')));
 // app.use(expr.static(path.join(__dirname, 'public')));
 // app.use(expr.static(path.join(__dirname, 'public/control')));
 
 
-app.use('/graphql', graphqlHTTP({
-    schema: roots.sStat,
-    rootValue: roots.rStat,
-    graphiql: true,
-}));
-
-
-
-//Routing
-app.use('/', def);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../tsclient/build/'))
+})
 
 db.once('open', () => {
     app.listen(PORT, () => {
